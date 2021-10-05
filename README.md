a3ec9f02a257512417256e078fced5ce82ff71b5
brepowell <85724630+brepowell@users.noreply.github.com>
2021-10-04 21:19:50 -0700

    Added more questions

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=5832241&assignment_repo_type=AssignmentRepo)
 
+//Breanna Powell
+//SID: 0641962
 
-Your repo must have a README.md file in which you:
-Summarize your scoring system
+Summarize your scoring system:
+For my scoring system, I would like to favor students who are younger, may not have an income, are veterans, or have high needs. My goal was to try and make the program fair for the students who use it. Students who are under disciplinary action are likely to be left out, while students who have high needs or less income are more likely to benefit from the algorithm.
+
+Higher points are given to students who are younger, have a lower class year, are veterans, or qualify for accomodations through Disability Resources for Students (DRS).
+
+For the part of the award scheme that is calculated, I chose to make it so that even if a student is older they can benefit from the algorithm if they have high needs or if they are a veteran.
+
 Sample output for three test cases:
 a 25 year old senior on academic probation
 a 22 year old junior who is a student teacher
@@ -11,6 +18,3 @@ Explain why your program is either fair or not fair. In particular, describe whi
 
 A student's priority is the sum of the points that can be assigned to them. So a junior (3 points) who is 23 years old (1 point) would have priority over a senior (4 points) who is on academic probation (-1 point).
 
-To reduce the utter triviality of this assignment, we introduce some additional requirements:
-
-You must have at least one point award in your scheme that is calculated, rather than determined directly by the answer (for example, you might assign points based on age by computing points = max(0, (age - 18) / 2)).
--- program0.cpp
+++ program0.cpp
@@ -6,60 +6,93 @@ using namespace std;
 //Point system for university housing lottery.
 //This program will ask students a series of questions and assign points based on the responses.
 //The points will determine what order students will be allowed to select housing.
-//Those with the most points get the first choice of housing.
+//Those students with the most points get the first choice of housing.
 
 int main()
 {
 
 //Question 1: Are you a Freshman, Sophomore, Junior, or Senior?
-//Current Freshman: 1 point
-//Current Sophomore: 2 points
-//Current Junior: 3 points
-//Current Senior: 4 points
-    int points = 0; 
-    cout << "Are you a Freshman, Sophomore, Junior, or Senior?" << endl;
-    cout << "Enter 1 for Freshman; 2 for Sophomore; 3 for Junior; 4 for Senior" << endl;
-    cin >> points; // Get user input from the keyboard
+//Current Freshman: 4 point
+//Current Sophomore: 3 points
+//Current Junior: 2 points
+//Current Senior: 1 points
+   int points = 0; 
+   cout << "Are you a Freshman, Sophomore, Junior, or Senior?" << endl;
+   cout << "Enter 1 for Freshman; 2 for Sophomore; 3 for Junior; 4 for Senior" << endl;
+   cin >> points; // Get user input from the keyboard
+   string answer = "";
+   switch(points){
+       case 1: 
+          answer = "Freshman";
+          break;
+       case 2: 
+          answer = "Sophomore";
+          break;
+       case 3: 
+          answer = "Junior";
+          break;
+       case 4: 
+          answer = "Senior";
+          break;
+   }
+   cout << "You entered " << answer << endl; // Display answer
+   points = 5-points; // enable Freshman students to get more points than Seniors
     
 //Question 2: What is your age in years?
-    int age = 0; 
-    cout << "What is your age in years?" << endl;
-    cin >> age; // Get user input from the keyboard
-    if (age >= 23) {
-        // if the student is older than 23 years, add 1 point
-        points++;
-    }
+   int age = 0; 
+   cout << "What is your age in years?" << endl;
+   cin >> age; // Get user input from the keyboard
+   cout << "You entered " << age << endl; // Display age
+   if (age <= 23) {
+      points++; // if the student is younger than 23 years, add 1 point
+   }
 
 //Question 3: Are you engaged in a Full-Time, Off-Campus Program (such as student teaching)?
-    int credits = 0;
-    string answer = "";
-    cout << "Are you engaged in a Full-Time, Off-Campus Program?" << endl;
-    cout << "Enter Yes or No" << endl;
-    cin >> answer;
-    answer == "Yes" || answer == "yes" ? credits++ : credits;
+   int credits = 0;
+   cout << "Are you engaged in a Full-Time, Off-Campus Program?" << endl;
+   cout << "Enter Yes or No" << endl;
+   cout << "You entered " << answer << endl; // Display answer
+   cin >> answer;
+   answer == "Yes" || answer == "yes" ? credits++ : credits;
 
 //Question 4: Are you you on Academic Probation, Possible Academic Suspension, or Disciplinary Probation?
 //Academic Probation: -1 point
 //Possible Academic Suspension: -2 points
 //On Disciplinary Probation at Any Point during the Academic Year: -3 points
-    int demerits = 0;
-    cout << "Are you you on Academic Probation, Possible Academic Suspension, or Disciplinary Probation?" << endl;
-    cout << "Enter Yes or No" << endl;
-    cin >> answer;
-    //Question that appears if a prior question is answered a certain way:
-    if (answer == "Yes" || answer =="yes"){
-        cout << "You answered " << answer <<endl;
-        cout << "Enter 1 if you are on Academic Probation" << answer <<endl;
-        cout << "Enter 2 if you are on Possible Academic Suspension" << answer <<endl;
-        cout << "Enter 3 if you are on Disciplinary Probation" << answer <<endl;
-        cin >> demerits;
-    }
+   int demerits = 0;
+   cout << "Are you you on Academic Probation, Possible Academic Suspension, or Disciplinary Probation?" << endl;
+   cout << "Enter Yes or No" << endl;
+   cin >> answer; // Get user input from the keyboard
+   cout << "You entered " << answer << endl; // Display answer
+   
+   //Question that appears if a prior question is answered a certain way:
+   if (answer == "Yes" || answer =="yes"){
+       cout << "Enter 1 if you are on Academic Probation" << answer <<endl;
+       cout << "Enter 2 if you are on Possible Academic Suspension" << answer <<endl;
+       cout << "Enter 3 if you are on Disciplinary Probation" << answer <<endl;
+       cin >> demerits;
+   }
+
+//Question 5: Do you qualify for accomodations through Disability Resources for Students (DRS)?
+   cout << "Do you qualify for accomodations through Disability Resources for Students (DRS)?" << endl;
+   cout << "Enter Yes or No" << endl;
+   cin >> answer; // Get user input from the keyboard
+   cout << "You entered " << answer << endl; // Display answer
+   if (answer == "Yes" || answer =="yes"){
+      credits += points/2; //Credits for this are calculated by taking the points for age
+   }
+
+//Question 6: Are you a veteran?
+   cout << "Do you qualify for accomodations through Disability Resources for Students (DRS)?" << endl;
+   cout << "Enter Yes or No" << endl;
+   cin >> answer; // Get user input from the keyboard
+   cout << "You entered " << answer << endl; // Display answer
+   answer == "Yes" || answer == "yes" ? credits++ : credits;
 
 //Add the credits to the total points and subtract the demerits:
-    points += credits - demerits;
+   points += credits - demerits;
 
 //Test:
-    cout << "You entereed " << answer << endl; // Display answer
-    cout << "You have " << points <<" housing points" << endl; // Display the points
+   cout << "You have " << points <<" housing points" << endl; // Display the points
 
 }
